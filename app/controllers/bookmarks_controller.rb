73f9ffc3c09c009dd43b974938ed626b220f0eae
Maximilian Steuck <max@fount.io>
2021-11-12 11:04:13 +0100

    heroku push

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,41 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark = Bookmark.new(bookmark_params)
+
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+
+    if @bookmark.save!
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    if @bookmark.destroy
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+rating =
+
+
+puts 'clear data'
+Movie.destroy_all
+
+puts 'insert data'
+
+50.times do
+  title = Faker::Movie.title
+  unless Movie.find_by(title: title)
+    Movie.create!(title: title,
+                  overview: Faker::Movie.quote,
+                  poster_url: "https://source.unsplash.com/1600x900/?#{title}",
+                  rating: rand * 10)
+  end
+end
+
+puts "created #{Movie.all.count}"
