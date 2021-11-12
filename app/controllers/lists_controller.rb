d66f15eef691647d5e2b4958f32701fc981e7d39
Maximilian Steuck <max@fount.io>
2021-11-12 15:43:15 +0100

    updates

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update]
   def index
     @lists = List.all
   end
@@ -13,8 +14,19 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    if @list.save!
+    if @list.save
       redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to list_path(@list)
     else
       render :new
     end
@@ -22,8 +34,12 @@ class ListsController < ApplicationController
 
   private
 
+  def set_list
+    @list = List.find(params[:id])
+  end
+
   def list_params
     # bookmark id?!
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,17 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  # dependent: :restrict_with_exception
   validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
-  # before_destroy :check_for_booksmarks
-
-
-  # private
-
-  # def check_for_booksmarks
-  #   if Bookmark.where(:movie_id).count.positive?
-  #     errors.add_to_base("cannot delete customer while orders exist")
-  #     return false
-  #   end
-  # end
 end
