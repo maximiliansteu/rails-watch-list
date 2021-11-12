005bc69346840c65fb497a201e886f3a5b1233ea
Maximilian Steuck <max@fount.io>
2021-11-12 16:36:41 +0100

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get root, to: 'lists#index'
-  resources :lists, except: [:index] do
+  resources :lists do
     resources :bookmarks, only: %i[index new create]
   end
   resources :bookmarks, only: [:destroy]
 end
+
+# except: [:index]
+# get root, to: 'lists#index'
